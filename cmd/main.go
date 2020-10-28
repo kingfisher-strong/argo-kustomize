1621d9b37d960f071561994741a5e4ed1feaf290
shashank0202 <snk0202@gmail.com>
2020-10-28 06:27:11 +0530

    testing ns creation

--- cmd/main.go
+++ cmd/main.go
@@ -25,7 +25,7 @@ func handler(w http.ResponseWriter, r *http.Request){
 	if err != nil {
 		host = "unknown host"
 	}
-	resp := fmt.Sprintf("testing echo $1 stdout --------- from           %s", host)
+	resp := fmt.Sprintf("testing echo $1 stdout and ns creation --------- from           %s", host)
 	_, err = w.Write([]byte(resp))
 	if err != nil {
 		log.Panicf("not able to write http output: %s", err)
